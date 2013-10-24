bd5ea28704452e8fc60323d4487672694233f3cd
art <art@skbkontur.ru>
2013-10-24 16:48:52 +0600

    TextExtFilter use OPT_ITEMS instead of 'filterItems'

--- src/js/textext.plugin.filter.js
+++ src/js/textext.plugin.filter.js
@@ -208,7 +208,7 @@
 	p.isValueAllowed = function(value)
 	{
 		var self        = this,
-			list        = self.opts('filterItems') || self._suggestions || [],
+			list        = self.opts(OPT_ITEMS) || self._suggestions || [],
 			itemManager = self.itemManager(),
 			result      = !self.opts(OPT_ENABLED), // if disabled, should just return true
 			i
